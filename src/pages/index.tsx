eeb69c82b4537d8ed04e7dc638d6c115ab64f20a
Accel <58154200+Accel-learn-how-to-code@users.noreply.github.com>
2021-05-01 11:56:17 +0700

    add some .env test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,14 @@ import Link from "next/link";
 export interface IndexProps {
   mircophones: Microphone[];
 }
-
 export default function Home({ mircophones }: IndexProps) {
+  console.log("bo", process.env.NEXT_PUBLIC_ENV_VARIABLE);
+  console.log("bo", process.env.ENV_VARIABLE);
   return (
     <div>
       <h2>Trang chu</h2>
+      <p>p {process.env.NEXT_PUBLIC_ENV_VARIABLE}</p>
+      <p>p2 {process.env.ENV_VARIABLE}</p>
       <Link href="/people">
         <a>Go to People Page</a>
       </Link>
@@ -25,5 +28,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   await new Promise((x) => {
     setTimeout(x, 3000);
   });
+  
   return { props: { mircophones } };
 };
